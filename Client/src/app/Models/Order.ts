04935b1bdcc988b7c540315845d74eeb04df2172
karabenemsi <karabenemsi@users.noreply.github.com>
2018-04-12 14:32:31 +0200

    Klasse Order hinzugefügt

---
+++ Client/src/app/Models/Order.ts
@@ -0,0 +1,26 @@
+
+class Order {
+  id: string;
+  title: string;
+  details: string;
+  number: number;
+  assignees: {
+    technical: string[],
+    coach: string[],
+    buisness: string[]
+  };
+  customer: string;
+  log: string[];
+  comment: string;
+  changes: {
+    date: Date;
+    event: string;
+    issuer: string;
+  }[];
+  dateInbox: Date;
+  dateAccepted: Date;
+  dateDone: Date;
+
+
+
+}
