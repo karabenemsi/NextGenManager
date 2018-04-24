dcff6806147e62b1c1b1d44fc30015b9214e5ba8
karabenemsi <karabenemsi@users.noreply.github.com>
2018-04-24 09:41:22 +0200

    Goto Loopback3

--- Server/index.js
+++
@@ -1,9 +0,0 @@
-const application = (module.exports = require('./dist'));
-
-if (require.main === module) {
-  // Run the application
-  application.main().catch(err => {
-    console.error('Cannot start the application.', err);
-    process.exit(1);
-  });
-}
